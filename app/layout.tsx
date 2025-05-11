c9efbc2059a5792091de40fd0c3c357adf206b92
sudeepbogati7 <sudeepbogati977@gmail.com>
2025-05-11 23:47:46 +0545

    recent news data population and client side data manipulation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
